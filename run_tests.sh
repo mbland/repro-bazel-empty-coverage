06711f7c20fa00f80984305d908f6ba62c8469d5
Mike Bland <mbland@engflow.com>
2026-02-27 09:31:17 -0500

    Replace `ls` with `diff` for expected coverage

--- run_tests.sh
+++ run_tests.sh
@@ -7,5 +7,9 @@ echo "Version: $(bazel --version)"
 
 set -ex
 bazel coverage --repo_env="SCALA_VERSION=${SCALA_VERSION}" //specs2_with_junit:foo_test
+diff specs2_with_junit/expected-coverage.dat \
+  bazel-testlogs/specs2_with_junit/foo_test/coverage.dat
+
 bazel coverage --repo_env="SCALA_VERSION=${SCALA_VERSION}" //scalatest:foo_test
-ls -l bazel-testlogs/{specs2_with_junit,scalatest}/foo_test
+diff scalatest/expected-coverage.dat \
+  bazel-testlogs/scalatest/foo_test/coverage.dat
