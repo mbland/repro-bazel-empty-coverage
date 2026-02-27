19005888d4a99c2adac6bd02a3ceed9f4efe74c4
Mike Bland <mbland@engflow.com>
2026-02-27 08:27:58 -0500

    Add Bazel path and version to run_test.sh

--- run_tests.sh
+++ run_tests.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
-set -ex
 SCALA_VERSION="${SCALA_VERSION:-2.12.21}"
 
+echo "Bazel:   $(command -v bazel)"
+echo "Version: $(bazel --version)"
+
+set -ex
 bazel coverage --repo_env="SCALA_VERSION=${SCALA_VERSION}" //specs2_with_junit:foo_test
 bazel coverage --repo_env="SCALA_VERSION=${SCALA_VERSION}" //scalatest:foo_test
 ls -l bazel-testlogs/{specs2_with_junit,scalatest}/foo_test
