4e666fab2c12835ed09e0133427bb3c3e3de7117
Mike Bland <mbland@engflow.com>
2026-02-27 17:56:35 -0500

    Make PROTOC_WORKAROUND a Bash array
    Fixes a problem whereby when it was the empty string, Bazel would
    interpret it as an empty target name and break.

--- run_tests.sh
+++ run_tests.sh
@@ -4,12 +4,12 @@ echo "Bazel:   $(command -v bazel)"
 BAZEL_VERSION="$(bazel --version)"
 echo "Version: ${BAZEL_VERSION}"
 
-PROTOC_WORKAROUND=""
+PROTOC_WORKAROUND=()
 
 if [[ "$BAZEL_VERSION" =~ ^bazel\ (10\.|no_version) ]]; then
   # For now, set this for Bazel prereleases to avoid recompiling `protoc`.
   # See: https://github.com/protocolbuffers/protobuf/issues/25561
-  PROTOC_WORKAROUND="--noincompatible_exclude_starlark_flags_from_exec_config"
+  PROTOC_WORKAROUND=("--noincompatible_exclude_starlark_flags_from_exec_config")
 fi
 
 set -ex
@@ -27,11 +27,11 @@ set -ex
 #
 # After first building `//scalatest:foo_test` by itself, adding it to this first
 # command will have no effect.
-bazel coverage "$PROTOC_WORKAROUND" //specs2_with_junit:foo_test
+bazel coverage "${PROTOC_WORKAROUND[@]}" //specs2_with_junit:foo_test
 diff specs2_with_junit/expected-coverage.dat \
   bazel-testlogs/specs2_with_junit/foo_test/coverage.dat
 
 # Add `--nocache_test_results` and the final `diff` will pass.
-bazel coverage "$PROTOC_WORKAROUND" //scalatest:foo_test
+bazel coverage "${PROTOC_WORKAROUND[@]}" //scalatest:foo_test
 diff scalatest/expected-coverage.dat \
   bazel-testlogs/scalatest/foo_test/coverage.dat
